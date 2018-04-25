0d74abf3da04df134a0faea65c1098ff3b192bd9
Lorena <lorena.giddens@outlook.com>
2018-04-25 20:37:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,10 @@ def won(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
     
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
     if board[win_index_1] == "X" &&
        board[win_index_2] == "X" && 
        board[win_index_3] == "X"
