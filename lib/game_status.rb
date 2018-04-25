ef71cbef76d89893f221c8681b08237caa1f3d9b
Lorena <lorena.giddens@outlook.com>
2018-04-25 20:39:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,10 +34,9 @@ def won(board)
        (position_1 == "O" &&
         position_2 == "O" &&
         position_3 == "O")
-      return win_combination
-    end
-  end
-  false
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
     end
   end
 end
