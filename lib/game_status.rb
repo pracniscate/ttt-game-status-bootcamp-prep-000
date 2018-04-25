762ac13fb8579f91258437502f80096447c510e3
Lorena <lorena.giddens@outlook.com>
2018-04-25 20:32:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,5 +24,12 @@ def won(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
     
-    if
+    if board[win_index_1] == "X" &&
+       board[win_index_2] == "X" && 
+       board[win_index_3] == "X"
+      return win_combo
+    elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
+      return win_combo
+    end
+  end
 end
